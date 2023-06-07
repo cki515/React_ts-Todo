2e1e290f7a5c5886ad59e7973786c75dab8d3993
cki515 <kwangilstory@gmail.com>
2023-06-07 14:06:34 +1200

    Initial setting

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'react', 'react-hooks', 'prettier'],
+  rules: {
+    'linebreak-style': 0,
+    'react/react-in-jsx-scope': 0,
+    'react/prefer-stateless-function': 0,
+    'react/jsx-filename-extension': 0,
+    'react/jsx-one-expression-per-line': 0,
+    'react/prop-types': 0,
+    'prettier/prettier': 0,
+    'import/no-unresolved': 0,
+    'import/no-extraneous-dependencies': 0,
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+};
---
+++ README.md
@@ -0,0 +1,30 @@
+# React TypeScript Todo
+
+## Install
+
+### project
+
+- npx create-react-app todolist --template typescript
+
+### router-dom
+
+- npm i react-router-dom
+- npm i -D @types/react-router-dom
+
+### eslint and plugin
+
+- npm i -D eslint eslint-config-airbnb eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react eslint-plugin-react-hooks
+- npx eslint --init
+
+### prettier
+
+- npm i -D prettier eslint-config-prettier eslint-plugin-prettier
+
+### styled-componenets
+
+- npm i styled-components@5.3.10
+- npm i -D @types/styled-components
+
+### redux
+
+- npm i @reduxjs/toolkit react-redux
